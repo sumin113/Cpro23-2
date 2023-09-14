e583f2b6b1d1e8d227df31c6888e16af274d988c
sumin113 <144310826+sumin113@users.noreply.github.com>
2023-09-14 16:25:33 +0900

    99

---
+++ 0914c/0914c/99.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	int i, j;
+
+	i = 2;
+	j = 1;
+
+	while (i<=9) {
+
+		if (i == 5) {
+			i++;
+			continue;
+		}
+
+		while (j <= 9) {
+			printf("%d * %d = %d\n", i, j, i * j);
+			j++;
+		}
+		i++;
+		j = 1;
+	}
+}
