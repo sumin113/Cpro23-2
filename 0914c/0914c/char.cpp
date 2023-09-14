0f83fda9698872972d6451f022147d0c761adfba
sumin113 <144310826+sumin113@users.noreply.github.com>
2023-09-14 14:42:10 +0900

    char

---
+++ 0914c/0914c/char.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	char ch;
+	char str[10];
+
+	printf("1, 문자입력 :");
+	scanf_s("%c", &ch, sizeof(ch));
+
+	printf("2 문자열 입력 :");
+	scanf_s("%s", &str, sizeof(str));
+
+	printf("문자 출력 : %c\n", ch);
+	printf("문자열 출력 : %s\n", str);
+
+	return 0;
+}
