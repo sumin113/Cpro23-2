22d26a5fe05f35d16444dca4ed300994ff63c0cf
sumin113 <144310826+sumin113@users.noreply.github.com>
2023-10-08 11:52:39 +0900

    Update 0921 lecture4.cpp

--- 0921/0921 lecture4.cpp
+++ 0921/0921 lecture4.cpp
@@ -1,24 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
+#include <math.h>
 
-int main(void) {
+int main(void)
+{
+    double x = 0, y = 0, r=0;
 
-	double x = 0;
-	double y = 0;
+    double cnt = 0, circle = 0;
 
-	int cnt = 0; 
-	int cir = 0;
+    int percent = 0, i=0;
 
-	srand(time(NULL));
+    srand(time(NULL));
 
-	while (cnt < 10000) {
-		x = (double)rand() / (double)RAND_MAX;
-		y = (double)rand() / (double)RAND_MAX;
-		cnt++;
-	}
+    while (cnt < 10000) {
+        x = (double)rand() / (double)RAND_MAX;
+        y = (double)rand() / (double)RAND_MAX;
+        cnt++;
 
-	return 0;
+        r = (x * x) + (y * y);
+
+        if (r <= 1) {
+            circle++;
+        }
+
+        percent = cnt / 100;
+
+        printf("%d%% 진행... 원주율 : %lf ", percent, (circle / cnt) * 4);
+        for (int i = 0; i < percent; i += 5)
+        {
+            printf("=");
+        }
+        printf("\n");
+
+    }
+
+    return 0;
 }
-
